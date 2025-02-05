8a53e8097344c493b5b662fd7dbcd720ac2b3226
brunoocn <bruno.caneo@contemplato.com>
2025-02-05 17:08:22 -0300

    feat: adicionando service de addresses

---
+++ src/modules/addresses/services/addresses.service.ts
@@ -0,0 +1,106 @@
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Address } from '../entities/adress.entity';
+import { CreateAddressDTO } from '../resolvers/dto/create-address.dto';
+import { UpdateAddressDTO } from '../resolvers/dto/update-address.dto';
+import { User } from 'src/modules/users/entities/user.entity';
+
+@Injectable()
+export class AddressesService {
+  constructor(
+    @InjectRepository(Address)
+    private readonly addressRepository: Repository<Address>,
+
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<Address[]> {
+    return this.addressRepository.find({ relations: ['user'] });
+  }
+
+  async findOne(id: number): Promise<Address> {
+    return this.addressRepository.findOne({
+      where: { id },
+      relations: ['user'],
+    });
+  }
+
+  async create(input: CreateAddressDTO): Promise<Address> {
+    const user = await this.userRepository.findOne({
+      where: { id: input.userId },
+      relations: ['addresses'],
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${input.userId} not found`);
+    }
+
+    if (this.verifyAddressExists(user.addresses, input.cep)) {
+      throw new BadRequestException(
+        `User already has an address registered with CEP: ${input.cep}`,
+      );
+    }
+
+    const address = this.addressRepository.create({ ...input, user });
+    return this.addressRepository.save(address);
+  }
+
+  async update(id: number, input: UpdateAddressDTO): Promise<Address> {
+    const address = await this.addressRepository.findOne({
+      where: { id },
+      relations: ['user'],
+    });
+
+    if (!address) {
+      throw new NotFoundException(`Address with ID ${id} not found`);
+    }
+
+    if (input.userId && input.cep) {
+      const user = await this.userRepository.findOne({
+        where: { id: input.userId },
+        relations: ['addresses'],
+      });
+
+      if (!user) {
+        throw new NotFoundException(`User with ID ${input.userId} not found`);
+      }
+
+      if (
+        this.verifyAddressExists(user.addresses, input.cep) &&
+        address.cep !== input.cep
+      ) {
+        throw new BadRequestException(
+          `User already has an address registered with CEP: ${input.cep}`,
+        );
+      }
+    }
+
+    Object.assign(address, input);
+    return this.addressRepository.save(address);
+  }
+
+  async delete(id: number): Promise<{ success: boolean; message: string }> {
+    const address = await this.addressRepository.findOne({ where: { id } });
+
+    if (!address) {
+      throw new NotFoundException(`Address with ID ${id} not found`);
+    }
+
+    await this.addressRepository.delete(id);
+    return { success: true, message: `Address with ID ${id} has been deleted` };
+  }
+
+  private verifyAddressExists(
+    userAddresses: Array<Address>,
+    inputCep: string,
+  ): boolean {
+    return userAddresses.some((address) => address.cep === inputCep);
+  }
+}
